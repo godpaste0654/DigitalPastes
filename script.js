c6184e8c9b94aeafeda8dd6077532bf1fe0dbb0d
godpaste0654 <166179195+godpaste0654@users.noreply.github.com>
2024-04-05 23:59:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function saveText() {
+    var text = document.getElementById('text-input').value;
+    // Puoi fare qualcosa qui con il testo inserito dall'utente, come ad esempio visualizzarlo
+    document.getElementById('output').innerText = text;
+}
